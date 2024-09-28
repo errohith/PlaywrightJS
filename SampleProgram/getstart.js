106a3507506461a94a79b1f99246507d2b953859
Rohith <errohith@gmail.com>
2024-09-28 09:22:38 +0530

    Commit Homework

--- SampleProgram/getstart.js
+++ SampleProgram/getstart.js
@@ -9,3 +9,10 @@ console.log("Data Type of mobileNumber" + typeof mobileNumber+ "value is:" +mobi
 console.log("Data Type of isAutomation" + typeof isAutomation+ "value is:" +isAutomation);
 console.log("Data Type of hasPlaywright" + typeof hasPlaywright+ "value is:" +hasPlaywright);
 
+var stock = -0.1;
+console.log(typeof stock)
+
+var page;
+console.log(typeof page);
+var input=null;
+console.log(typeof input);
---
+++ SampleProgram/homeworkassignment.js
@@ -0,0 +1,17 @@
+const browserVersion="Chrome";
+function getBrowserVersion(){
+if(browserVersion.startsWith("Chrome")){
+
+    const  browserVersion="3.14";
+    console.log("Inside the if block " +browserVersion);
+   
+}else{
+    console.log("Browser version is other browsers");
+
+}
+
+console.log(browserVersion);
+
+}
+
+getBrowserVersion();
