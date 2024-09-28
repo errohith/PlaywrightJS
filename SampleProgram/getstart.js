89f78bab195aa876163a39960a2c7f05e0d17ec2
Rohith <errohith@gmail.com>
2024-09-28 09:13:17 +0530

    Commit Base

---
+++ SampleProgram/getstart.js
@@ -0,0 +1,11 @@
+var firstName = "Rohithkumar";
+var companyName = "Telstra";
+var mobileNumber = 9632587410;
+var isAutomation = false;
+var hasPlaywright;
+console.log("Data Type of FirstName" + typeof firstName +" value is :" +firstName);
+console.log("Data Type of companyName" + typeof companyName+ "value is :" +companyName);
+console.log("Data Type of mobileNumber" + typeof mobileNumber+ "value is:" +mobileNumber);
+console.log("Data Type of isAutomation" + typeof isAutomation+ "value is:" +isAutomation);
+console.log("Data Type of hasPlaywright" + typeof hasPlaywright+ "value is:" +hasPlaywright);
+
